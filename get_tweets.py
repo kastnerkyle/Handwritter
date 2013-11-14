9b0cf47bb528743b7bb6f36e296ca5e294d947e2
Kyle Kastner <kastnerkyle@gmail.com>
2013-11-13 19:23:20 -0600

    Updated get_tweets

--- get_tweets.py
+++ get_tweets.py
@@ -15,6 +15,22 @@ from Queue import Empty
 import itertools
 import twitter
 
+class Tweet(dict):
+    def __init__(self, tweet_json):
+        super(Tweet, self).__init__(self)
+        if tweet_json and 'delete' not in tweet_json:
+            self['timestamp'] = dateutil.parser.parse(tweet_json[u'created_at']).replace(tzinfo=None).isoformat()
+            self['text'] = tweet_json['text']
+            self['hashtags'] = [x['text'] for x in tweet_json['entities']['hashtags']]
+            self['geo'] = tweet_json['geo']['coordinates'] if tweet_json['geo'] else None
+            self['id'] = tweet_json['id']
+            self['screen_name'] = tweet_json['user']['screen_name']
+            self['user_id'] = tweet_json['user']['id']
+
+    def __str__(self):
+       return json.dumps(self, indent=2, sort_keys=True)
+
+
 #Read oAuth credentials from symlinked twitter_creds.txt file
 """
 Expected file structure for twitter_creds.txt is:
@@ -26,7 +42,10 @@ token_secret:XXXX
 """
 
 with open("twitter_creds.txt") as f:
-    twitter_creds = {line.split(":")[0]:line.split(":")[1].strip() for line in f}
+     twitter_creds = {}
+     for line in f:
+         k,v = line.strip().split(":")
+         twitter_creds[k] = v
 
 twitter_stream = twitter.TwitterStream(auth=twitter.OAuth(
             token=twitter_creds["token"],
@@ -35,5 +54,5 @@ twitter_stream = twitter.TwitterStream(auth=twitter.OAuth(
             consumer_secret=twitter_creds["consumer_secret"]))
 
 stream = twitter_stream.statuses.sample(block=True)
-tweet = stream.next()
-print json.dumps(tweet, indent=2, sort_keys=True)
+print Tweet(stream.next())
+#json.dumps(tweet, indent=2, sort_keys=True)
